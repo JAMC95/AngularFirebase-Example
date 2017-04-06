5145f4e91760665af48cb5efac2dbc5fbc3b5af7
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-04-06 12:32:06 +0200

    Used the same template to add for Edit listing Put information in the textfield

--- src/app/components/edit-listing/edit-listing.component.ts
+++ src/app/components/edit-listing/edit-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-listing',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-listing.component.css']
 })
 export class EditListingComponent implements OnInit {
+  id;
+  title;
+  owner;
+  city;
+  bedrooms;
+  price;
+  image;
+  type;
 
-  constructor() { }
+  constructor(
+    private firebaseService:FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getListingDetails(this.id).subscribe(listing => {
+      this.title = listing.title;
+      this.owner = listing.owner;
+      this.city = listing.city;
+      this.bedrooms = listing.bedrooms;
+      this.price = listing.price;
+      this.image = listing.image;
+      this.type = listing.type;
+    })
   }
 
 }
